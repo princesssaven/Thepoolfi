9c9a77ac64f2c98db07bd4d750a2ebf3fa990c65
thisisbigmike <egbo2255@gmail.com>
2026-03-12 12:58:18 -0400

    chore: remove repository files

--- src/agent/protocols/base.ts
+++
@@ -1,6 +0,0 @@
-import { PortfolioState, ProtocolOpportunity } from "@/agent/types";
-
-export interface ProtocolAdapter {
-  readonly name: "jupiter" | "marinade" | "raydium";
-  fetchOpportunities(portfolio: PortfolioState): Promise<ProtocolOpportunity[]>;
-}
